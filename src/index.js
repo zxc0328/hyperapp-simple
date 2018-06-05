79c0c5da2a09886ed5fc2e7e5fff25fe88108973
zxc0328 <120797887@qq.com>
2018-06-05 20:49:21 +0800

    feat: add data

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import { h, app } from "hyperapp";
 
-fetch("/api/topics/hot.json")
-  .then(res => {
-    return res.json();
-  })
-  .then(re => {
-    console.log(re);
-  });
+const List = ({ lists, actions }) => (
+  <div
+    oncreate={() => {
+      fetch("/api/topics/hot.json")
+        .then(res => {
+          return res.json();
+        })
+        .then(re => {
+          actions.setList(re);
+        });
+    }}
+  >
+    {lists.map(item => {
+      return <li>{item.title}</li>;
+    })}
+  </div>
+);
 
 const state = {
-  count: 0
+  lists: []
 };
 
 const actions = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
+  setList: value => state => ({ lists: value })
 };
 
 const view = (state, actions) => (
   <div>
-    <h1>{state.count}</h1>
-    <button onclick={() => actions.down(1)}>-</button>
-    <button onclick={() => actions.up(1)}>+</button>
+    <List lists={state.lists} actions={actions} />
   </div>
 );
 
